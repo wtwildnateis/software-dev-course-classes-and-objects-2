65273a6148849b9bf03491e8f367f1a9f3c72926
Michael Lambert <mlambert125@live.com>
2025-02-10 09:33:37 -0500

    ...

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,6 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("This program doesn't do anything yet.  Run Unit tests to verify your code.");
     }
 }
